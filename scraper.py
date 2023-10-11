86429ee0fde8b651fb72a325c0af32d15ba4a47d
yujing0131 <snake830131@gmail.com>
2023-10-11 17:45:42 +0800

    5.6 - 批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 from selenium.webdriver.common.action_chains import ActionChains
 
 ##利用驅動程式管理員在執行排重拾自動下載驅動程式
 driver=  webdriver.Chrome()
-
 
 driver.get('https://shopee.tw/mall/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
 time.sleep(5)
@@ -16,11 +17,24 @@ ActionChains(driver).move_by_offset(100,100).click().perform()
 cards = driver.find_elements(By.CSS_SELECTOR,"div[class='Qnex0a']")
 #將彈跳視窗關閉
 for card in cards:
-    ActionChains(driver).move_to_element(card).perform()
-    title = card.find_element(By.CSS_SELECTOR,"div[class='WF8zKZ XNhH9V vy4NFA']").text #商品名稱
-    price = card.find_element(By.CSS_SELECTOR,"div[class='CCRieW _7BzXXp']").text#商品價格
+    # ActionChains(driver).move_to_element(card).perform()
+    title = card.find_element(By.CSS_SELECTOR,"div[class='WF8zKZ _8ayTha _3I+cDt']").text #商品名稱
+    price = card.find_element(By.CSS_SELECTOR,"div[class='CCRieW ZJwBnM']").text#商品價格
     link = card.find_element(By.TAG_NAME,"a").get_attribute('href')##求出<a>內的gref屬性質
     items.append((title,price,link))
 
-print(items)
-    
+
+result = []
+for item in items:
+
+    driver.get(item[2])
+    # time.sleep(5)
+    for i in range(5):##設定滾動滑鼠的次數
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(3)
+    comments = driver.find_elements(By.CSS_SELECTOR,"div[class='Rk6V+3']")
+    for comment in comments:
+        result.append((item[0],item[1],comment.text))
+    break
+
+print(result)
